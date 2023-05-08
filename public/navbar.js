39b44045a1e6cb88fee59828ab4b50c18f29cdaf
Tobias Diez <tobias@diez.guru>
2023-05-08 16:44:16 +0200

    account route fixed

--- public/navbar.js
+++ public/navbar.js
@@ -26,7 +26,7 @@ function NavBar(){
             <a className="nav-link" href="#/alldata/">AllData</a>
           </li>     
           <li className="nav-item">
-            <a className="nav-link" href="#/account/">Account</a>
+            <a className="nav-link" href="account/">Account</a>
           </li>       
         </ul>
       </div>
