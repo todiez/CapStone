9ac73bb65087c27e598586eedf1e0db7130fd0f6
Tobias Diez <tobias@diez.guru>
2023-05-10 14:28:35 +0200

    before context edits

--- public/login.js
+++ public/login.js
@@ -1,6 +1,6 @@
-function Login(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');    
+function Login() {
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState("");
 
   return (
     <Card
@@ -8,62 +8,83 @@ function Login(){
       txtcolor="black"
       header="Login"
       status={status}
-      body={show ? 
-        <LoginForm setShow={setShow} setStatus={setStatus}/> :
-        <LoginMsg setShow={setShow} setStatus={setStatus}/>}
+      body={
+        show ? (
+          <LoginForm setShow={setShow} setStatus={setStatus} />
+        ) : (
+          <LoginMsg setShow={setShow} setStatus={setStatus} />
+        )
+      }
     />
-  ) 
+  );
 }
 
-function LoginMsg(props){
-  return(<>
-    <h5>Success</h5>
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={() => props.setShow(true)}>
+function LoginMsg(props) {
+  return (
+    <>
+      <h5>Success</h5>
+      <button
+        type="submit"
+        className="btn btn-light"
+        onClick={() => props.setShow(true)}
+      >
         Authenticate again
-    </button>
-  </>);
+      </button>
+    </>
+  );
 }
 
-function LoginForm(props){
-  const [email, setEmail]       = React.useState('');
-  const [password, setPassword] = React.useState('');
+function LoginForm(props) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-  function handle(){
+  function handle() {
     fetch(`/account/login/${email}/${password}`)
-    .then(response => response.text())
-    .then(text => {
+      .then((response) => response.text())
+      .then((text) => {
         try {
-            const data = JSON.parse(text);
-            props.setStatus('');
-            props.setShow(false);
-            console.log('JSON:', data);
-        } catch(err) {
-            props.setStatus(text)
-            console.log('err:', text);
+          const data = JSON.parse(text);
+          props.setStatus("");
+          props.setShow(false);
+          console.log("JSON:", data);
+        } catch (err) {
+          props.setStatus(text);
+          console.log("err:", text);
         }
-    });
+      });
   }
 
+  return (
+    <>
+      Email
+      <br />
+      <input
+        type="input"
+        className="form-control"
+        placeholder="Enter email"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+      <br />
+      Password
+      <br />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Enter password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+      <br />
+      <button type="submit" className="btn btn-light" onClick={handle}>
+        Login
+      </button>
+    </>
+  );
+}
 
-  return (<>
-
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} 
-      onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    Password<br/>
-    <input type="password" 
-      className="form-control" 
-      placeholder="Enter password" 
-      value={password} 
-      onChange={e => setPassword(e.currentTarget.value)}/><br/>
-
-    <button type="submit" className="btn btn-light" onClick={handle}>Login</button>
-   
-  </>);
+function Logincorner(props) {
+ 
+  let show = true;
+  return <>{show ? <p>Paz@diez.cc</p> : <p>You are not logged in</p>}</>;
 }
