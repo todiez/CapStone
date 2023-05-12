19c48d8d32e143859c47fb2c9aa129b57ca4deb3
Tobias Diez <tobias@diez.guru>
2023-05-12 15:56:16 +0200

    Logout centered

--- public/alldata.js
+++ public/alldata.js
@@ -11,8 +11,19 @@ function LoggOut() {
   
 
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center" }}>
       <h5>Successfully logged out.</h5>
-    </>
+      <button
+        type="button"
+        className="btn btn-dark"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = "./#/login/";
+        }}
+      >
+        Go to Login
+      </button>
+    </div>
   );
+  
 }
